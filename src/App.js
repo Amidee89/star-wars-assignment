ca02b03f452fd6338d3a705e988b8f623987d155
Marco Carandente <marco.carandente@gmail.com>
2023-08-25 22:37:01 +0300

    Visualizing points on globe
    Using globe-gl

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import Globe from 'react-globe.gl';
+
 import { useEffect, useState } from "react";
 const API_URL = "https://aseevia.github.io/star-wars-frontend/data/secret.json";
 
 function App() {
   const [decodedMessage, setDecodedMessage] = useState(null);
+  const [worldData, setWorldData] = useState([]);
 
   const getSecret = async () => {
     const response = await fetch(`${API_URL}`);
     const data = await response.json();
     const base64Decoded = atob(data.message);
+    const parsedData = JSON.parse(base64Decoded);
     setDecodedMessage(base64Decoded);
-    console.log('Base64 Decoded:', base64Decoded);
+    setWorldData(parsedData);  // Updating worldData
 
   }		
   useEffect (()=>{
@@ -21,8 +25,17 @@ function App() {
   
   return (
     <div className="App">
-      <h1>Star Wars Secret Decoder</h1>
-      <p>Decoded message: {decodedMessage}</p>
+<div style={{ width: '100vw', height: '100vh' }}>
+      <Globe
+        globeImageUrl="//unpkg.com/three-globe/example/img/earth-day.jpg"
+        pointsData={worldData}
+        pointLat="lat"
+        pointLng="long"
+        pointColor={() => 'red'}
+        pointAltitude={0}
+        pointRadius={1.005}
+      />
+    </div>
     </div>
   );
 }
