01a2589a25bb2ca352bae13998ffcbd780e674cc
Marco Carandente <marco.carandente@gmail.com>
2023-08-27 15:20:59 +0300

    Load additional data
    All data is now loaded and merged with spread operator

--- src/App.js
+++ src/App.js
@@ -4,24 +4,38 @@ import Globe from 'react-globe.gl';
 
 import { useEffect, useState} from "react";
 const API_URL = "https://aseevia.github.io/star-wars-frontend/data/secret.json";
-
+const ADDITIONAL_INFO_URL = "https://akabab.github.io/starwars-api/api/id/";
 function App() {
-  const [decodedMessage, setDecodedMessage] = useState(null);
   const [worldData, setWorldData] = useState([]);
   const [width, setWidth] = useState(window.innerWidth);
   const [height, setHeight] = useState(window.innerHeight);
 
-  const getSecret = async () => {
+  const getData = async () => {
     const response = await fetch(`${API_URL}`);
     const data = await response.json();
     const base64Decoded = atob(data.message);
     const parsedData = JSON.parse(base64Decoded);
-    setDecodedMessage(base64Decoded);
-    setWorldData(parsedData);  // Updating worldData
-
+    
+    // Fetch additional data for each item by ID
+    const promises = parsedData.map(async (item) => {
+      const additionalData = await getAdditionalData(item.id);
+      return { ...item, ...additionalData };
+    });
+    
+    const updatedData = await Promise.all(promises);
+    
+    console.log(updatedData);
+    setWorldData(parsedData);
   }		
+  
+  const getAdditionalData = async (id) => {
+    const response = await fetch(`${ADDITIONAL_INFO_URL}${id}.json`);
+    const data = await response.json();
+    return data;
+  };
+  
   useEffect (()=>{
-    getSecret();
+    getData();
     window.addEventListener('resize', handleResize);
   },[]);
   
